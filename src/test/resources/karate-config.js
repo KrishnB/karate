11d7c9c5ca9e3b9f6a700b9f4b5751ff4b97f309
krishnb <kbandame@gmail.com>
2017-08-10 17:47:20 +0530

    Updated config.js:

--- src/test/resources/karate-config.js
+++ src/test/resources/karate-config.js
@@ -1,11 +1,3 @@
 function() {
     karate.configure('ssl',true);
-     var config = { // base config
-        username: 'ramana',
-        password: 'demo',
-        grant_type: 'password',
-        scope:'read',
-        tenantId:"default"
-      };
-      return config;
 }
